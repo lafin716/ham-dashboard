1ae3caf39272f98414c9fe6426ca86a1eae3ca19
lafinco <lafin716@naver.com>
2022-03-24 00:23:50 +0900

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/_include/Header';
+import Sidebar from './components/_include/Sidebar';
+import Footer from './components/_include/Footer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Intro from './components/intro/Intro';
+import AppList from './components/app/AppList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Sidebar />
+        <Switch>
+          <Route path="/app"><AppList /></Route>
+          <Route path="/"><Intro /></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
